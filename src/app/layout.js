cfc5b4758153f3f77fc04e56bc581b23fa01e4f3
Carolina Mattos <128762797+nanahmattos@users.noreply.github.com>
2026-04-02 14:04:30 -0400

    feat: add google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -35,6 +35,15 @@ export default function RootLayout({ children }) {
         <Header />        
         {children}
         <Analytics />
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q59XK9HG6N"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-Q59XK9HG6N');
+        </script>
       </body>
     </html>
   );
